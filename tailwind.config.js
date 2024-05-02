828965feb0f7f189f614efca1e1a8e6bc9f36884
Richard Musime <richardmusime6@gmail.com>
2024-05-02 17:56:14 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["public/index.html"],
+    theme: {
+      extend: {colors: {
+        redish: '#c30a17',
+        
+      },
+    width:{
+      
+        width: "100%"
+    },
+  height:{
+    height: "110vh",
+  } },
+    },
+    plugins: [],
+  }
+  
+  
